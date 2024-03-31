e654ee40ab22673b19d1ac5abe7a28f92ce47764
Roman Larionov <pcsaveddotcom@gmail.com>
2024-03-31 11:23:13 -0400

    model file updated to reflect URLField for image

--- cars/models.py
+++ cars/models.py
@@ -8,8 +8,8 @@ class Car(models.Model):
     mileage = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    
-    photo = models.ImageField(upload_to='cars', default='cars/default.jpeg', blank=True)
+    # Change this line
+    photo = models.URLField(max_length=200, default='', blank=True)
 
     def __str__(self):
         return f"{self.make} {self.model} {self.year}"
