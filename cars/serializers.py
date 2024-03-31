0deee69aa0e6ca6b1a77f9f50b23c47fd689516d
Roman Larionov <pcsaveddotcom@gmail.com>
2024-03-31 13:19:50 -0400

    serializer updated to support URLs as images

--- cars/serializers.py
+++ cars/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import Car
 
 class CarSerializer(serializers.ModelSerializer):
+
+    photo = serializers.ImageField(required=False)
+
     class Meta:
         model = Car
         fields = '__all__'
