c2800524649c3b976f51b844a74db66bcd49aaf4
Roman Larionov <pcsaveddotcom@gmail.com>
2024-03-31 13:11:05 -0400

    car model updated with URLs that can be rendered on FE

--- cars/management/commands/seedcars.py
+++ cars/management/commands/seedcars.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
                 'mileage': 15000,
                 'price': 20000,
                 'description': 'A reliable car',
-                'photo': 'https://ibb.co/ZJ447xL'
+                'photo': 'https://live.staticflickr.com/65535/53623319538_82e45d8d94_m.jpg'
             },
             {
                 'make': 'Honda',
@@ -25,7 +25,7 @@ class Command(BaseCommand):
                 'mileage': 10000,
                 'price': 18000,
                 'description': 'Compact and fuel-efficient',
-                'photo': 'https://ibb.co/J281CbZ'
+                'photo': 'https://live.staticflickr.com/65535/53623560995_0386e3e449_m.jpg'
             },
             # Add more cars here (!!!)
         ]
--- cars/models.py
+++ cars/models.py
@@ -8,8 +8,8 @@ class Car(models.Model):
     mileage = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    # Change this line
-    photo = models.URLField(max_length=200, default='', blank=True)
+    # Use ImageField instead of URLField
+    photo = models.ImageField(upload_to='cars', default='https://live.staticflickr.com/65535/53623560975_05981be63f_m.jpg', blank=True)
 
     def __str__(self):
         return f"{self.make} {self.model} {self.year}"
