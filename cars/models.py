d8df3fe79a3bfedcbf2161e7dedc3948c27fcb2d
Roman Larionov <pcsaveddotcom@gmail.com>
2024-03-31 13:36:16 -0400

    models updated photo field

--- cars/models.py
+++ cars/models.py
@@ -8,7 +8,7 @@ class Car(models.Model):
     mileage = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    photo = models.ImageField(upload_to='cars', default='live.staticflickr.com/65535/53623560975_05981be63f_m.jpg', blank=True)
+    photo = models.ImageField(upload_to='cars', default='default.jpg', blank=True)
 
     def __str__(self):
         return f"{self.make} {self.model} {self.year}"
