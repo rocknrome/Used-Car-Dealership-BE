c88d699bbe93454956e73ab7175312c845ee243a
Roman Larionov <pcsaveddotcom@gmail.com>
2024-04-08 00:35:28 -0400

    updated cors

--- dealership/settings.py
+++ dealership/settings.py
@@ -114,6 +114,6 @@ CORS_ALLOWED_ORIGINS = [
     'https://used-car-dealership-fe.vercel.app',
     'https://magenta-cheesecake-a549ae.netlify.app',
     'https://golden-daifuku-b91655.netlify.app',
-    'http://localhost:64350'
+    'http://localhost:50260'
 ]
 
