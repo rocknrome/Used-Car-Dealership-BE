a69da0ab58f8cc68f81805f35cda2020f699dd77
Roman Larionov <pcsaveddotcom@gmail.com>
2024-04-09 19:43:49 -0400

    allowed all origins in CORS for testing

--- dealership/settings.py
+++ dealership/settings.py
@@ -116,6 +116,7 @@ CORS_ALLOWED_ORIGINS = [
     'https://golden-daifuku-b91655.netlify.app',
     'http://localhost:50260',
     'https://final-project-flutter-car-club.vercel.app/',
-    'https://rocknrome0409.web.app'
+    'https://rocknrome0409.web.app',
+     '*' # allow all origins
 ]
 
