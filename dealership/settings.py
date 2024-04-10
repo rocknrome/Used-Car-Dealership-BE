ebb7121b104cbc9c3cc56474a6387036d8c61b68
Roman Larionov <pcsaveddotcom@gmail.com>
2024-04-10 11:18:31 -0400

    settings port updated

--- dealership/settings.py
+++ dealership/settings.py
@@ -117,7 +117,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:50260',
     'https://final-project-flutter-car-club.vercel.app/',
     'https://rocknrome0409.web.app',
-    'http://localhost:63957/',
+    'http://localhost:51193/',
      '*' # allow all origins
 ]
 
